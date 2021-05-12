15fde2638349875738b9b61a6416ec4f1d6d5060
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-05-12 11:18:06 -0700

    using headers and helper method decodeparams to process returned data in params

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const url = require('url');
 const axios = require('axios');
 const chalk = require('chalk');
 const config = require('./config');
+
+const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET'
+};
+
+const decodeParams = searchParams => Array
+    .from(searchParams.key())
+    .reduce((acc, key) => (
+        { ...acc, [key]: searchParams.get(key) }
+    ), {});
