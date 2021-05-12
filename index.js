8bc5fa527a1bacdea205c1b003c46a0086f45b36
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-05-12 13:31:41 -0700

    axois is set up to fetch url and parse returned data

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ const decodeParams = searchParams => Array
     .reduce((acc, key) => (
         { ...acc, [key]: searchParams.get(key) }
     ), {});
+
+const server = createServer((req, res) => {
+    const requestURL = url.parse(req.url);
+    const decodeParams = decodeParams(new url.URLSearchParams(requestURL.search));
+    const { search, location, country = 'us'} = decodeParams;
+
+    const targetURL = `${config.BASE_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.API_KEY}&what=${search}&where=${location}`;
+    if (req.method === 'GET') {
+        console.log(chalk.green(`proxy GET request to: ${targetURL}`));
+        axios.get(targetURL)
+            .then(response => {
+                response.writeHead(200, headers);
+                response.end(JSON.stringify(response.data));
+            })
+            .catch(error => {
+                console.log(chalk.red(error));
+                response.writeHead(500, headers);
+                response.end(JSON.stringify(error));
+            })
+    }
+})
